de311a45e718649e723f79a9e8aaf2c8d8e2a488
Joseph Fabrizzio <115313375+JFabrizzio5@users.noreply.github.com>
2024-09-22 20:24:04 -0600

    Codigo en ingles
    Version en ingles por que pues luego lo pone asi el profe

--- Unidad 1/Tarea2_EstruccturasArreglosCiclos/Codigo2.kt
+++ Unidad 1/Tarea2_EstruccturasArreglosCiclos/Codigo2.kt
@@ -1,37 +1,35 @@
-package Tarea2_EstructurasArreglosYCiclos
-
-//created by: 2CV22 HERNANDEZ GONZALEZ JOSEPH FABRIZZIO
-fun calcularPromedio(calificaciones: Array<Double>): Double {
-    return calificaciones.average()
+// Created by: 2CV22 HERNANDEZ GONZALEZ JOSEPH FABRIZZIO
+fun calculateAverage(grades: Array<Double>): Double {
+    return grades.average()
 }
 
-fun calificacionMasAlta(calificaciones: Array<Double>): Double {
-    return calificaciones.maxOrNull() ?: 0.0
+fun highestGrade(grades: Array<Double>): Double {
+    return grades.maxOrNull() ?: 0.0
 }
 
-fun calificacionMasBaja(calificaciones: Array<Double>): Double {
-    return calificaciones.minOrNull() ?: 0.0
+fun lowestGrade(grades: Array<Double>): Double {
+    return grades.minOrNull() ?: 0.0
 }
 
-fun reprobados(alumnos: Array<String>, calificaciones: Array<Double>): List<Pair<String, Double>> {
-    return alumnos.zip(calificaciones).filter { it.second < 60.0 }
+fun failingStudents(students: Array<String>, grades: Array<Double>): List<Pair<String, Double>> {
+    return students.zip(grades).filter { it.second < 60.0 }
 }
 
 fun main() {
-    val alumnos = arrayOf("José", "Alberto", "Laura", "Noel", "Erika", "Daniel")
-    val calificaciones = arrayOf(31.0, 94.0, 98.5, 75.0, 46.5, 75.0)
+    val students = arrayOf("José", "Alberto", "Laura", "Noel", "Erika", "Daniel")
+    val grades = arrayOf(31.0, 94.0, 98.5, 75.0, 46.5, 75.0)
 
-    val promedio = calcularPromedio(calificaciones)
-    val maxCalificacion = calificacionMasAlta(calificaciones)
-    val minCalificacion = calificacionMasBaja(calificaciones)
-    val alumnosReprobados = reprobados(alumnos, calificaciones)
+    val average = calculateAverage(grades)
+    val maxGrade = highestGrade(grades)
+    val minGrade = lowestGrade(grades)
+    val failing = failingStudents(students, grades)
 
-    println("Promedio de calificación: $promedio")
-    println("Calificación más alta: $maxCalificacion")
-    println("Calificación más baja: $minCalificacion")
+    println("Promedio de calificación: $average")
+    println("Calificación más alta: $maxGrade")
+    println("Calificación más baja: $minGrade")
 
     println("Reprobados:")
-    for ((alumno, calificacion) in alumnosReprobados) {
-        println("$alumno, $calificacion")
+    for ((student, grade) in failing) {
+        println("$student, $grade")
     }
 }
